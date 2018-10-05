1d9fc8726ed733e91cab848b6bb2d2bb2ed9b32c
hsputra <syahputra.hsp@gmail.com>
2018-10-05 18:42:10 +0700

    upload to npmjs

--- index.ts
+++ index.ts
@@ -1,4 +1,12 @@
 import cardano = require ("rust-cardano-crypto");
 
+export default {
+    ...cardano
+}
+// export * from 'rust-cardano-crypto';
+// import cardano = require ("rust-cardano-crypto");
+// console.log("ngaaacoooo -->", cardano.);
 
-console.log("ngaaacoooo -->", cardano.PaperWallet);
+// export function cardanoABC(){
+//     return 'tst';
+// // }
